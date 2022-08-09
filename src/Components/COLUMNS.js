6dfc7e214531a183271a7e8dfb37357c21d20c87
Meryem Toulali <95414856+sweatyfbiprofiler@users.noreply.github.com>
2022-08-09 17:44:53 +0100

    table still not working

---
+++ src/Components/COLUMNS.js
@@ -0,0 +1,22 @@
+export const COLUMNS = [
+    {
+        Header: "Id",
+        accessor: "id",
+    },
+    {
+        Header: "Image",
+        accessor: "image",
+    },
+    {
+        Header: "City",
+        accessor: "city",
+    },
+    {
+        Header: "Country",
+        accessor: "country",
+    },
+    {
+        Header: "Postal code",
+        accessor: "postal_code",
+    },
+];
--- src/Pages/CommunesVilles.js
+++ src/Pages/CommunesVilles.js
@@ -1,11 +1,113 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
+import axios from "axios";
+import ReactTable from "react-table";
+import { useTable } from "react-table";
+import { COLUMNS } from "../Components/COLUMNS";
+import { useState } from "react";
 
+/*
+const vCommunesVilles = () => {
+    const [cities, setCities] = useState();
+    const [loading, setLoading] = useState(true);
+
+    const url = "http://demo5246547.mockable.io/";
+
+
+    useEffect( () => {
+
+       const doFetch = async (url) => {
+            const response = await axios.get(url);
+            //console.log(response.data);
+            setCities(response.data);
+
+            setLoading(false);
+        
+    }
+       
+        
+
+        doFetch(url);
+
+        
+    }, [])
+    
+    
+    console.log(cities);
+
+    return(
+        <div className="main-container">ok</div>
+    )
+
+
+    
+}*/
+
+function Table({ columns, data }) {
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+        useTable({
+            columns,
+            data,
+        });
+
+    return (
+        <div>
+            <table {...getTableProps()}>
+                <thead>
+                    {headerGroups.map((headerGroup) => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map((column) => (
+                                <th {...column.getHeaderProps()}>
+                                    {column.render("Header")}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {rows.map((row) => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map((cell) => {
+                                    return (
+                                        <td {...cell.getCellProps()}>
+                                            {cell.render("Cell")}
+                                        </td>
+                                    );
+                                })}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
+}
 
 class CommunesVilles extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { cities: [] };
+    }
+
+    componentDidMount() {
+        this.doFetch("http://demo5246547.mockable.io/");
+    }
+
+    doFetch = async (url) => {
+        const response = await axios.get(url);
+
+        this.setState({ cities: response.data });
+        console.log(this.state.cities);
+    };
+
     render() {
         return (
             <div className="main-container">
-                Communes / villes
+                {" "}
+                rendered
+                <ReactTable data={this.state.cities} columns={COLUMNS} />
             </div>
         );
     }
